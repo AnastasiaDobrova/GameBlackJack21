584ffe610889ddb44923dd920f2f19c14131ac6f
Anastasia <anastasia.dobrova@mail.ru>
2020-09-19 11:35:54 +0200

    Beginning

---
+++ app/src/main/java/com/example/gameblackjack21/PlayersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gameblackjack21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_players)
+    }
+}
